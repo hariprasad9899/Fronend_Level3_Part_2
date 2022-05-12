a3e385c2705f1c85994afadd54eb0fed595f0a05
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-05-13 03:15:22 +0530

    sorting done

--- static-job-listings-master/App/app.js
+++ static-job-listings-master/App/app.js
@@ -53,10 +53,13 @@ window.onload = function() {
         let pElem = [...skills.getElementsByTagName('p')];
         pElem[0].innerText = jobData.role;
         pElem[1].innerText = jobData.level;
+        myNode.dataset.skills += jobData.role + " ";
+        myNode.dataset.skills += jobData.level + " ";
         let skillSet = jobData.languages.concat(jobData.tools)
         for (let val of skillSet) {
             let pNode = document.createElement('p');
             pNode.innerText = val;
+            myNode.dataset.skills += val + " ";
             skills.appendChild(pNode);
         }
     }
@@ -67,12 +70,113 @@ window.onload = function() {
             let allNode = document.getElementsByClassName('all')[0];
             let job = document.getElementsByClassName('job')[0];
             let clonedJobNode = job.cloneNode(1);
+            clonedJobNode.classList.remove('spl');
             clonedJobNode.style.display = "flex";
+            clonedJobNode.dataset.skills = ""
             setElements(jobData,clonedJobNode);
             setSkills(jobData,clonedJobNode);
             allNode.appendChild(clonedJobNode);
+        }
+        let updatedJobs = document.getElementsByClassName('job');
+        return updatedJobs;
+    }
 
+   
+    function appliedFilters() {
+        const values = document.getElementsByClassName('values')[0];
+        let tabletElem = [...values.getElementsByClassName('tablet')]
+        let filterElems = [];
+        for(let each of tabletElem) {
+            let pElem = each.getElementsByTagName('p')[0];
+            filterElems.push(pElem.textContent)
+        }
+        return filterElems;
+    }
+
+    async function sortData() {
+        let allJobs = [...document.getElementsByClassName('openjobs')];
+        let filterElem = appliedFilters();
+        for(let eachJob of allJobs) {
+            if(!eachJob.classList.contains('spl')) {
+                let jobSkills =  eachJob.dataset.skills;
+                for(let filter of filterElem) {
+                    if(!jobSkills.includes(filter)) {
+                        eachJob.style.display = "none";
+                    }
+                }
+            }
         }
     }
-    fillData();
+    
+    function reverseSortData() {
+        let allJobs = [...document.getElementsByClassName('job')];
+        let filterElem = appliedFilters();
+        console.log(filterElem.length)
+        for(let eachJob of allJobs) {
+            if(filterElem.length > 0) {
+                if(!eachJob.classList.contains('spl')) {
+                    let jobSkills =  eachJob.dataset.skills;
+                    for(let filter of filterElem) {
+                        if(jobSkills.includes(filter)) {
+                            eachJob.style.display = "flex";
+                        } else {
+                            eachJob.style.display = "none";
+                        }
+                    }
+                }
+            } else if(!eachJob.classList.contains('spl')) {
+                eachJob.style.display = "flex";
+            } 
+        }
+    }
+
+
+    function addFilters(val) {
+        let addedFilters = appliedFilters();
+        if(addedFilters.indexOf(val) > 0) {
+            return 
+        } else {
+        const values = document.getElementsByClassName('values')[0];
+        let myDiv = document.createElement('div');
+        myDiv.className = "tablet";
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        p2.className = "closeSelected"
+        p1.innerText = val;
+        p2.innerHTML = '&times;'
+        myDiv.appendChild(p1);
+        myDiv.appendChild(p2);
+        values.appendChild(myDiv);
+        closebtn();
+        sortData();
+        }
+    }
+
+    async function addData() {
+        let updatedJobs = await fillData();
+        let myUpdatedJobs = [...updatedJobs];
+        for (let eachElem of myUpdatedJobs) {
+            let mySkills = eachElem.getElementsByClassName('mySkills')[0];
+            let skills = [...mySkills.getElementsByTagName('p')];
+            for(let i of skills) {
+                i.addEventListener('click', ()=> {
+                    addFilters(i.innerText)
+                })
+            }
+        }
+    }
+
+    
+    let closebtn = function() {
+        const closeSelected = [...document.getElementsByClassName('closeSelected')];
+        closeSelected.forEach(elem => {
+            elem.addEventListener('click', () => {
+                elem.parentElement.remove();
+                reverseSortData();
+            })
+        })
+    }
+    closebtn();
+
+    addData();
 }
